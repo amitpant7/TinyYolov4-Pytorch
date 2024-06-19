5d75829920d1379b9c1a29eabdcb514450121352
Amit <amitrajpant7@gmail.com>
2024-06-19 17:04:52 +0545

    transforms correct

--- utils/transform.py
+++ utils/transform.py
@@ -31,6 +31,7 @@ val_transformations = v2.Compose(
         v2.RandomResizedCrop(size=(416, 416), scale=(0.9, 1), antialias=True),
         v2.ToDtype(torch.float32, scale=True),  # Normalize expects float input
         v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        v2.SanitizeBoundingBoxes(),
         FinalTranform(),
     ]
 )
